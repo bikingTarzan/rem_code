6d69a07238cf80282ad07b0f0905f17aec644aca
bikingTarzan <272867557@qq.com>
2018-10-28 11:47:25 +0800

    对象的类型检测

---
+++ rem_code_181028/class_def.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+__date__ = '2018/10/28 11:44'
+
+
+class Persion:
+    pass
+
+p = Persion()
+
+print(type(p) is Persion)     #True
+
+print(type(p) == Persion)     #True
+
+print(isinstance(p, Persion))     #True
