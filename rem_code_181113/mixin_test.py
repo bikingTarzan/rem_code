72b8cbb339751a087ca2294558f4d59072c9b26b
bikingTarzan <272867557@qq.com>
2018-11-13 23:15:07 +0800

    mixin实现，python3默认广度遍历

--- rem_code_181113/mixin_test.py
+++ rem_code_181113/mixin_test.py
@@ -6,10 +6,13 @@ class Vehicle(object):
     def run(self):
         print("I can run")
 
+    def fly(self):
+        print('I am flying, Vehicle')
+
 
 class PlaneMixin(object):
     def fly(self):
-        print('I am flying')
+        print('I am flying, Plane')
 
 
 class Airplane(Vehicle, PlaneMixin):
@@ -20,5 +23,5 @@ if __name__ == "__main__":
     airplane = Airplane()
     print(Airplane.__mro__)    #(<class '__main__.Airplane'>, <class '__main__.Vehicle'>, <class '__main__.PlaneMixin'>, <class 'object'>)
     airplane.run()      #I can run
-    airplane.fly()      #I am flying
+    airplane.fly()      #I am flying, Vehicle    #广度遍历，fly方法在Vehicle就先找到并返回了。
 
